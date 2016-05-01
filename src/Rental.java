feda0ace1c5cf3705e2ba8f521cbf1760714cf8d
Heiko Faller <heiko_faller@t-online.de>
2016-05-01 13:28:17 +0200

    moved method "getFrequentRenterPoints" from "Rental" to "Movie"

--- src/Rental.java
+++ src/Rental.java
@@ -16,10 +16,6 @@ class Rental {
         return movie.getCharge(daysRented);
     }
     public int getFrequentRenterPoints() {
-        int frequentRenterPoints = 1 ;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-            frequentRenterPoints ++;
-        return frequentRenterPoints;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
